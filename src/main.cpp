e6e0f8ad3d3be9ba6f584a5c22e29ade4347462d
Lukejoanderson <lukejoanderson@gmail.com>
2026-01-06 23:11:52 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+
+// put function declarations and global variables here:
+
+WiFiServer server(80);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); //for debug
+  Serial.write("Hello World");
+  WiFi.softAP("MSOE-ASME-BOT","123456789");
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function (and class) definitions here:
