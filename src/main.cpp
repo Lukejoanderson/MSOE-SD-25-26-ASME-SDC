7252d1c5f846a9ec8dcd541bd278bff1e5b94405
Lukejoanderson <lukejoanderson@gmail.com>
2026-01-09 16:42:24 -0600

    Webpage opens @ 1.2.3.4

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,24 @@
 
 // put function declarations and global variables here:
 
+IPAddress local_IP(1,2,3,4);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
 WiFiServer server(80);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); //for debug
   Serial.println("Hello World");
+  WiFi.softAPConfig(local_IP,gateway,subnet);
   WiFi.softAP(WifiName,Pword);
   server.begin();
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
   WiFiClient client=server.available();
-  
   if (client.available())
   {
     int bytes=client.available();
@@ -29,9 +34,13 @@ void loop() {
       message[i]=client.read();
     }
     Serial.write(message);
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println("Connection: close");
+    client.println();
+    client.println("<!DOCTYPE html><html><body><h1>Hi</h1></body></html>");
+    client.println();
   }
 }
 
 // put function (and class) definitions here:
-
-//test
